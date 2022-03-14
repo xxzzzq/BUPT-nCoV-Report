7ae2d00aff1dd41970a7e93849ea4481953bae72
rn7s2 <mail@rn7s2.cn>
2022-03-14 13:35:30 +0800

    Alpha.

--- README.md
+++ README.md
@@ -6,8 +6,18 @@
 # BUPT-nCOV-Report
 ## 北京邮电大学疫情防控打卡
 
-使用GitHub Actions定时打卡。
+使用 GitHub Actions 定时打卡。
 
 目前支持晨午晚检，后续可能增加功能。
 
-欢迎Issue和PR.
+欢迎 Issue 和 PR.
+
+## 使用方法
+0. Fork 本仓库
+0. 在你 Fork 的仓库的 Settings 的 Secrets 中设置以下信息:
+- `IS_AT_SCHOOL`: 0或1，是否在校
+- `BUPT_USERNAME`: 学号
+- `BUPT_PASSWORD`: 密码
+
+## 失败警告
+如果打卡失败，GitHub 会发邮件到你的邮箱。
--- main.ps1
+++ main.ps1
@@ -1,3 +1,7 @@
+if (!$env:IS_AT_SCHOOL -or !$env:BUPT_USERNAME -or $env:BUPT_PASSWORD) {
+    throw "Secrtes参数未设置，请参考README."
+}
+
 $session = New-Object Microsoft.PowerShell.Commands.WebRequestSession;
 $session.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36 Edg/99.0.1150.39";
 
@@ -30,7 +34,31 @@ if ($res.StatusCode -ne 200 -or (ConvertFrom-Json $res.Content).e -ne 0) {
     throw "登录失败";
 }
 
-Write-Host "登录成功，开始晨午晚检打卡";
+Write-Host "登录成功，开始获取现存填报数据";
+
+$res = Invoke-WebRequest -UseBasicParsing -Uri "https://app.bupt.edu.cn/xisuncov/wap/open-report/index" `
+    -WebSession $session `
+    -Headers @{
+    "sec-ch-ua"          = "`" Not A;Brand`";v=`"99`", `"Chromium`";v=`"99`", `"Microsoft Edge`";v=`"99`""
+    "Accept"             = "application/json, text/plain, */*"
+    "X-Requested-With"   = "XMLHttpRequest"
+    "sec-ch-ua-mobile"   = "?0"
+    "sec-ch-ua-platform" = "`"Windows`""
+    "Sec-Fetch-Site"     = "same-origin"
+    "Sec-Fetch-Mode"     = "cors"
+    "Sec-Fetch-Dest"     = "empty"
+    "Referer"            = "https://app.bupt.edu.cn/site/ncov/xisudailyup"
+    "Accept-Encoding"    = "gzip, deflate, br"
+    "Accept-Language"    = "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6"
+};
+
+$content = ConvertFrom-Json($res.Content);
+if ($res.StatusCode -ne 200 -or $content.e -ne 0) {
+    throw "现存填报数据获取失败，手动填报一次可能会解决问题";
+}
+$data = $content.d.info;
+
+Write-Host "获取现存填报数据成功，开始晨午晚检打卡";
 
 $res = Invoke-WebRequest -UseBasicParsing -Uri "https://app.bupt.edu.cn/xisuncov/wap/open-report/save" `
     -Method "POST" `
@@ -51,13 +79,13 @@ $res = Invoke-WebRequest -UseBasicParsing -Uri "https://app.bupt.edu.cn/xisuncov
 } `
     -ContentType "application/x-www-form-urlencoded" `
     -Body @{
-    "sfzx"         = 0
+    "sfzx"         = $env:IS_AT_SCHOOL
     "tw"           = 1
-    "area"         = "陕西省 渭南市 临渭区"
-    "city"         = "渭南市"
-    "province"     = "陕西省"
-    "address"      = "陕西省渭南市临渭区杜桥街道东风大街6号恒昌王子国际酒店"
-    "geo_api_info" = ConvertFrom-Json '{"type":"complete","position":{"Q":34.499926215278,"R":109.49075412326397,"lng":109.490754,"lat":34.499926},"location_type":"html5","message":"Get ipLocation failed.Get geolocation success.Convert Success.Get address success.","accuracy":18,"isConverted":true,"status":1,"addressComponent":{"citycode":"0913","adcode":"610502","businessAreas":[{"name":"朝阳大街","id":"610502","location":{"Q":34.492806,"R":109.476858,"lng":109.476858,"lat":34.492806}}],"neighborhoodType":"","neighborhood":"","building":"","buildingType":"","street":"东风大街","streetNumber":"6号","country":"中国","province":"陕西省","city":"渭南市","district":"临渭区","towncode":"610502001000","township":"杜桥街道"},"formattedAddress":"陕西省渭南市临渭区杜桥街道东风大街6号恒昌王子国际酒店","roads":[],"crosses":[],"pois":[],"info":"SUCCESS"}';
+    "area"         = $data.area
+    "city"         = $data.city
+    "province"     = $data.province
+    "address"      = $data.address
+    "geo_api_info" = $data.geo_api_info
     "sfcyglq"      = 0
     "sfyzz"        = 0
     "qtqk"         = ''
